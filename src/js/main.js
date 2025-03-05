e931c12bfce67e11e43dd41a5e6b02e1ae06e0a4
gosoomdoce@gmail.com <gosoomdoce@gmail.com>
2025-03-05 16:28:22 +0900

    25-03-05 인트로 제작 완료!
    패럴랙스 효과 추가!~

--- src/js/main.js
+++ src/js/main.js
@@ -28,7 +28,19 @@ window.onload = function() {
 
 
     function vanilaJavascripts() {
+        function intro_parallax() {
+            window.addEventListener("scroll", function () {
+                let scrollValue = window.scrollY;
+                let its_me = document.querySelector('#its-me-wrapper > div')
+                
+                //document.getElementById("intro-parallax-div").style.transform = `translateY(${scrollValue * 0.3}px)`;
+                its_me.style.transform = `translate(0, ${scrollValue * 0.3}px)`;
+                document.getElementById("take-a-look").style.transform = `translate(-50%, ${scrollValue * 0.2}px)`;
+            });
+        }
 
+        
+        intro_parallax();
     };
 
 
