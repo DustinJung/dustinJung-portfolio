8bf1857d4b30d237960cba401c6f97629ac822dd
gosoomdoce@gmail.com <gosoomdoce@gmail.com>
2025-03-07 12:17:47 +0900

    25-03-07 skill 섹션 카드 구현

--- src/js/components.js
+++ src/js/components.js
@@ -1,7 +1,15 @@
 /* components.js => 컴포넌트( 슬라이더, 모달, UI 등 ) 관리 js 파일 */
 
 function initSwiper() {
-
+    var swiper = new Swiper(".skill-swiper", {
+        slidesPerView: 3.5,
+        spaceBetween: 30,
+        centeredSlides: true,
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+      });
 }
 
 function initUIs() {
--- src/js/main.js
+++ src/js/main.js
@@ -19,6 +19,7 @@ function startWeb() {
 startWeb();
 
 
+
 document.addEventListener("DOMContentLoaded", function () {
     initUtils();
 });
@@ -48,31 +49,92 @@ window.onload = function() {
             let theTextBox = document.querySelector('#my-projects-section > .this-planet-wrapper');
             let thePlanet = document.querySelector('.the-planet');
             let timeout;
-            let inPlanet = document.querySelector('.in-the-planet')
+            let inPlanet = document.querySelector('#in-the-planet');
 
-            thePlanet.addEventListener('mouseenter', function() {
-                clearTimeout(timeout);
-                theTextBox.classList.add('on');
-            })
+            function planetHover() {
+                thePlanet.addEventListener('mouseenter', function() {
+                    clearTimeout(timeout);
+                    theTextBox.classList.add('on');
+                })
+    
+                thePlanet.addEventListener('mouseleave', function() {
+                    timeout = setTimeout(() => {
+                        theTextBox.classList.remove('on');
+                    }, 150);
+                })
+            }
+            function planetClick() {
+                thePlanet.addEventListener('click', function() {
+                    this.classList.add('on');
+                    setTimeout(() => {
+                        theTextBox.style = `display: none`;
+                        this.style = `touch-action: none; cursor: auto;`;
+                    }, 3500);
+                    
+                    setTimeout(() => {
+                        this.classList.add('nowGo');
+                        inPlanet.classList.add('on');
+                        if(inPlanet.classList.contains('on')) {
+                            document.querySelector('body').classList.remove('is-ready');
+                        };
+                        setTimeout(() => {
+                            inPlanet.classList.add('goOn');
+                        }, 50);
+                    },3700);
 
-            thePlanet.addEventListener('mouseleave', function() {
-                timeout = setTimeout(() => {
-                    theTextBox.classList.remove('on');
-                }, 150);
-            })
+                })
+            }
+            function createParticles() {
+                let container = document.querySelector('.the-particles');
+                for (let i = 0; i < 20; i++) { //for문 돌리고
+                    let particle = document.createElement('div'); //for문마다 particle이라는 변수의 div를 create(20개)
+                    particle.classList.add('particle'); //그 div에 particle class를 추가.
+                    let size = Math.random() * 5 + 2; // Math.random은 0 - 1 사이 랜덤값 생성/ 거기에 *5 + 2(2는 최소 크기)
+                    let posX = Math.random() * window.innerWidth; //innerWidth는 브라우저 화면 가로 너비
+                    let posY = Math.random() * window.innerHeight; // innerHeight => 브라우저 화면 세로 너비
+                    let duration = Math.random() * 5 + 3; // 애니메이션 지속 시간도 랜덤하게 설정.
+                    particle.style.width = `${size}px`;
+                    particle.style.height = `${size}px`;
+                    particle.style.left = `${posX}px`;
+                    particle.style.top = `${posY}px`;
+                    particle.style.animationDuration = `${duration}s`;
+                    container.appendChild(particle); //container 안에 자식요소로 particle을 추가
+                }
+            }
 
-            thePlanet.addEventListener('click', function() {
-                this.classList.add('on');
-                setTimeout(() => {
-                    this.classList.add('nowGo')
-                    inPlanet.classList.add('on')
-                },3700);
-                setTimeout(() => {
-                    theTextBox.style = `display: none`;
-                    this.style = `touch-action: none; cursor: auto;`;
-                }, 3500)
-            })
+            function theCardEffect() {
+                let containers = document.querySelectorAll('.skill-swiper-wrapper .container');
+                let overlays = document.querySelectorAll('.skill-swiper-wrapper .overlay');
+                // 1. 각 콘테이너와 overlay의 index가 같음
+                // 2. 각 contianer 안에서 forEach 돌려야함
+                // 3. 그 안에서 container에 index값 hover하면 동일한 index값 overlay의 backgroundposition이 바뀌어야 함
+                // 4. 해결 방법 => forEach에서 index값 주고, 그 안에서 overlays에 i값 배열에 이벤트 주기
+                containers.forEach((container, i) => {
+                    container.addEventListener('mousemove', function(e) {
+                        let x = e.offsetX;
+                        let y = e.offsetY; // 마우스 x, y값 가져오기
+            
+                        let rotateY = (-1/5) * x + 20; // X축 회전 조정 / 최솟값과 최댓값이 20
+                        let rotateX = 4/30 * y - 20;   // Y축 회전 조정 / x보다 y가 긴 만큼 수치 적용 방향 다르게 적용
+            
 
+                        overlays[i].style.backgroundPosition = `${x/5 + y/5}%`; // 백그라운드 위치 조정, 마우스 위치보다 1/5 씩.
+
+                        container.style.transform = `perspective(600px) rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
+                    });
+            
+                    container.addEventListener('mouseout', function() {
+                        overlays[i].style.opacity = "0";
+                        container.style.transform = `perspective(600px) rotateY(0deg) rotateX(0deg)`;
+                    });
+                })
+            }
+
+
+            planetHover();
+            planetClick();
+            createParticles();
+            theCardEffect()
         }
 
 
